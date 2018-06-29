6f32c893da0e75ca322e8bc0dab8479a03be9518
Michael Priebe <PriebeMichaelA@gmail.com>
2018-06-29 17:25:37 -0400

    Change so a window is not left behind

--- multisearch/multisearch.js
+++ multisearch/multisearch.js
@@ -1,21 +1,29 @@
 let params = location.search
-
+window.close()
 if (params == '') {
     window.open('./popup.html')
 } else {
     params = params.slice(1).split('&').map(p => p.split('='))
+    let first = false
     function next() {
         let param = params.shift()
         if (!param) return
         let key = param[0]
         let value = param[1]
         if (!key || !value) return next()
+        let url
         if (key.includes('google')) {
-            window.open('https://www.google.com/search?q=' + value)
+            url = 'https://www.google.com/search?q=' + value
         } else if (key.includes('bing')) {
-            window.open('https://www.bing.com/search?q=' + value)
+            url = 'https://www.bing.com/search?q=' + value
+        }
+        if (!first) {
+            first = url
+        } else {
+            window.open(url)
         }
         next()
     }
     next()
+    window.location = first
 }
