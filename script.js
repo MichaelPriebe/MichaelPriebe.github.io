958cf993eefca8f1a82bbe1c200099324b8366a7
MichaelPriebe <priebemichaela@gmail.com>
2022-12-04 22:19:06 -0500

    Refactor

--- script.js
+++ script.js
@@ -13,60 +13,34 @@ const ceil = Matter.Bodies.rectangle(0, 0, thickness, thickness, {
   isStatic: true,
 });
 
-const mouseConstraint = Matter.MouseConstraint.create(engine, {
-  element: document.body,
-});
+Matter.Composite.add(engine.world, [floor, left, right, ceil]);
 
-Matter.Composite.add(engine.world, [floor, left, right, ceil, mouseConstraint]);
-
-class LinkBallElement {
+class PhysicsElement {
   constructor(element) {
+    element.addEventListener("mousedown", this.mouseDown.bind(this));
+    element.addEventListener("touchstart", this.mouseDown.bind(this));
+    element.addEventListener("mousemove", this.mouseMove.bind(this));
+    element.addEventListener("touchmove", this.mouseMove.bind(this));
+    element.addEventListener("click", this.mouseUp.bind(this));
+    element.addEventListener("touchend", this.mouseUp.bind(this));
     this.element = element;
-    this.element.addEventListener("mousedown", this.mouseDown.bind(this));
-    this.element.addEventListener("touchstart", this.mouseDown.bind(this));
-    this.element.addEventListener("mousemove", this.mouseMove.bind(this));
-    this.element.addEventListener("touchmove", this.mouseMove.bind(this));
-    this.element.addEventListener("click", this.mouseUp.bind(this));
-    this.element.addEventListener("touchend", this.mouseUp.bind(this));
 
-    const size = Math.random() * 20 + 20;
-    const style = this.element.style;
-    style.width = size + "vmin";
-    style.height = size + "vmin";
-
-    this.body = Matter.Bodies.circle(
-      window.innerWidth / 2,
-      window.innerHeight / 2,
-      this.element.clientWidth / 2
+    const width = element.clientWidth;
+    const height = element.clientHeight;
+    const body = Matter.Bodies.rectangle(
+      element.offsetLeft + width / 2,
+      element.offsetTop + height / 2,
+      width,
+      height
     );
-    Matter.Composite.add(engine.world, this.body);
+
+    Matter.Composite.add(engine.world, body);
+    this.width = width;
+    this.height = height;
+    this.body = body;
+
     Matter.Events.on(engine, "beforeUpdate", this.beforeUpdate.bind(this));
     Matter.Events.on(engine, "afterUpdate", this.afterUpdate.bind(this));
-  }
-
-  clampPosition() {
-    const { x, y } = this.body.position;
-    Matter.Body.setPosition(this.body, {
-      x: Matter.Common.clamp(x, 0, window.innerWidth),
-      y: Matter.Common.clamp(y, 0, window.innerHeight),
-    });
-  }
-
-  beforeUpdate() {
-    const body = this.body;
-    const scale = this.element.clientWidth / 2 / body.circleRadius;
-    if (scale == 1) return;
-    Matter.Body.scale(body, scale, scale);
-  }
-
-  afterUpdate() {
-    this.clampPosition();
-    const style = this.element.style;
-    const body = this.body;
-    const { x, y } = body.position;
-    style.left = x - body.circleRadius + "px";
-    style.top = y - body.circleRadius + "px";
-    style.transform = "rotate(" + body.angle + "rad)";
   }
 
   mouseDown(event) {
@@ -88,6 +62,39 @@ class LinkBallElement {
     event.preventDefault();
     this.clicked = false;
   }
+
+  beforeUpdate() {
+    const element = this.element;
+    const width = element.clientWidth;
+    const height = element.clientHeight;
+    Matter.Body.scale(this.body, width / this.width, height / this.height);
+    this.width = width;
+    this.height = height;
+  }
+
+  afterUpdate() {
+    const body = this.body;
+    const { x, y } = body.position;
+    const element = this.element;
+    const style = element.style;
+    const width = element.clientWidth;
+    const height = element.clientHeight;
+    const tx = -element.offsetLeft - width / 2 + x;
+    const ty = -element.offsetTop - height / 2 + y;
+    style.translate = `${tx}px ${ty}px`;
+    style.transform = "rotate(" + body.angle + "rad)";
+  }
+}
+
+function render() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  Matter.Body.setPosition(floor, { x: width / 2, y: height + thickness / 2 });
+  Matter.Body.setPosition(left, { x: -thickness / 2, y: height / 2 });
+  Matter.Body.setPosition(right, { x: width + thickness / 2, y: height / 2 });
+  Matter.Body.setPosition(ceil, { x: width / 2, y: -thickness / 2 });
+  Matter.Engine.update(engine);
+  requestAnimationFrame(render);
 }
 
 window.addEventListener("deviceorientation", function (event) {
@@ -109,19 +116,14 @@ window.addEventListener("deviceorientation", function (event) {
   }
 });
 
-function render() {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  Matter.Body.setPosition(floor, { x: width / 2, y: height + thickness / 2 });
-  Matter.Body.setPosition(left, { x: -thickness / 2, y: height / 2 });
-  Matter.Body.setPosition(right, { x: width + thickness / 2, y: height / 2 });
-  Matter.Body.setPosition(ceil, { x: width / 2, y: -thickness / 2 });
-  Matter.Engine.update(engine);
-  requestAnimationFrame(render);
-}
-
-const html = document.querySelector("html");
-html.classList.remove("no-js");
-html.classList.add("js");
-document.querySelectorAll(".link").forEach((e) => new LinkBallElement(e));
-render();
+window.addEventListener("load", function () {
+  const html = document.documentElement;
+  const mouseConstraint = Matter.MouseConstraint.create(engine, {
+    element: html,
+    angularStiffness: 0,
+  });
+  html.style.overflow = "hidden";
+  Matter.Composite.add(engine.world, mouseConstraint);
+  document.querySelectorAll(".physics").forEach((e) => new PhysicsElement(e));
+  render();
+});
